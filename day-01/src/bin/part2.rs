38d3bf3cfe8bb682d65bf8935433839722f8cbf0
S P <palutz@users.noreply.github.com>
2023-12-02 23:11:37 +0000

    Update Day 1 README.md

--- day-01/src/bin/part2.rs
+++ day-01/src/bin/part2.rs
@@ -1,4 +1,26 @@
+const name_to_ num : Vec<(&str, &str>)> =
+    vec![
+        ("uno", "1"),
+    ];
+
+fn part2(input: &str) -> i32 {
+    let lines = input.trim().split('\n');
+    let total: i32 = lines
+                        .map(extract_integers)
+                        .sum();
+
+    total
+}
+
 fn main() {
     let input = include_str!("./input01.txt");
-    println!("{input}");
+    let res2 = part2(input);
+    println!("{res1}");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test() {}
 }
