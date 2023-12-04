8fc14d556aed6b9fbc69c61fa16b67245e3ac0f9
palutz <stefano13@duck.com>
2023-12-04 22:01:45 +0000

    day 1 part 2 tests

--- day-01/src/bin/part2.rs
+++ day-01/src/bin/part2.rs
@@ -2,3 +2,21 @@ fn main() {
     let input = include_str!("./input01.txt");
     println!("{input}");
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test() {
+        let s1 = "two1nine";
+        let actual = extract_integers(s1);
+        assert_eq!(actual, 12);
+
+        // let s2 = "eightwothree";
+        // let s3 = "abcone2threexyz";
+        // let s4 = "xtwone3four";
+        // let s5 = "4nineeightseven2";
+        // let s6 = "zoneight234";
+        // let s7 = "7pqrstsixteen";
+    }
+}
